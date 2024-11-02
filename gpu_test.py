f6879af9bd69afa844367d4c7db4e5871fb6b9a0
aryaandperimeter <sharm215@msu.edu>
2024-11-01 20:20:46 -0400

    Add gpu_test.py to verify GPU availability on HPCC

--- gpu_test.py
+++ gpu_test.py
@@ -1,4 +1,9 @@
-# gpu_test.py
+# gpu_check.py
 import torch
+
 print("CUDA Available:", torch.cuda.is_available())
 print("CUDA Device Count:", torch.cuda.device_count())
+if torch.cuda.is_available():
+    print("CUDA Device Name:", torch.cuda.get_device_name(0))
+else:
+    print("No GPU available.")
